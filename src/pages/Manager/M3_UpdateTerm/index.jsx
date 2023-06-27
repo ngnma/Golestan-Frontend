e092ac0773f90039fdb2bac17c51561272a5dbf3
ngnma <n.mashayekhi456@gmail.com>
2023-06-27 15:38:39 +0330

    M3 ui added

--- src/pages/Manager/M3_UpdateTerm/index.jsx
+++ src/pages/Manager/M3_UpdateTerm/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './style.scss';
 import DashboardLayout from '../../../components/DashboardLayout';
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, TextField, Button } from "@mui/material";
+import { Margin } from '@mui/icons-material';
 
 export default function M3_UpdateTerm(props) {
   const { index } = props;
@@ -18,10 +19,8 @@ export default function M3_UpdateTerm(props) {
 
     // </DashboardLayout>
     <DashboardLayout>
-      <Grid container>
-        <Grid
-          container
-          item
+      <Grid container >
+        <Grid item container 
           justifyContent="space-between"
           alignContent="center"
         >
@@ -30,15 +29,65 @@ export default function M3_UpdateTerm(props) {
             <Typography>{index}ویرایش اطلاعات ترم ؟</Typography>
           </Grid>
         </Grid>
-        <hr
+        <hr 
           style={{
             width: "100%",
             borderWidth: 0,
             borderTop: "1px solid black",
           }}
         />
-        <Grid item>
 
+        <Grid container >
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4} container gap="20%" 
+          >
+            <Grid item container direction="column">
+              <Grid item>
+                <label>نام ترم</label>
+              </Grid>
+              <Grid item >
+                <TextField variant="outlined" style={{ width: "100%" }} />
+              </Grid>
+            </Grid>
+
+            <Grid item container direction="column">
+              <Grid item>
+                <label>لیست اساتید</label>
+              </Grid>
+              <Grid item container gap="10px">
+                <Grid item flexGrow={1}>
+                  <Button variant="contained" className='M3_btn'>آپلوداکسل</Button>
+                </Grid>
+                <Grid item flexGrow={1}>
+                  <Button variant="contained" className='M3_btn'>افزودن</Button>
+                </Grid>
+                <Grid item flexGrow={5}>
+                  <TextField variant="outlined" style={{ width: "100%" }} />
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item container direction="column">
+              <Grid item>
+                <label>لیست دانشجویان</label>
+              </Grid>
+              <Grid item container gap="10px">
+                <Grid item flexGrow={1}>
+                  <Button variant="contained" className='M3_btn'>آپلوداکسل</Button>
+                </Grid>
+                <Grid item flexGrow={1}>
+                  <Button variant="contained" className='M3_btn'>افزودن</Button>
+                </Grid>
+                <Grid item flexGrow={5}>
+                  <TextField variant="outlined" style={{ width: "100%" }} />
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Button itme variant="contained" style={{ width: "100%", height: "50px"}}>ذخیره تغییرات</Button>
+
+          </Grid>
+          <Grid item xs={4}></Grid>
         </Grid>
       </Grid>
     </DashboardLayout>
