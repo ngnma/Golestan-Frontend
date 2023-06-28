272bba04e32e7aeb8d2beef4bad7d6ebb96ff9c4
ngnma <n.mashayekhi456@gmail.com>
2023-06-29 02:34:06 +0330

    Manager Frontend completed

--- src/pages/Manager/M4_CoursesList/index.jsx
+++ src/pages/Manager/M4_CoursesList/index.jsx
@@ -1,10 +1,11 @@
-import { Button } from '@mui/base';
+
 import React from 'react';
 import Mainlayout from '../../../components/MainLayout';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import * as action from "../../../action"
+import { Grid, Button } from '@mui/material';
 
 export default function M4_CoursesList() {
     const dispatch = useDispatch();//redux
@@ -20,10 +21,16 @@ export default function M4_CoursesList() {
     }
 
     return (
-        //in bayad ba redux bashe.az m2 miad esm term
         <Mainlayout text={termName}>
-            <Button variant="contained" onClick={goToM5}>preRegisterd</Button>
-            <Button variant="contained" onClick={goToM8}>Registerd</Button>
+
+            <div style={{ display: "flex", flexFlow: "1", gap: "50px", justifyContent: "space-between" }} >
+                <div >
+                    <Button variant="contained" style={{ width: "200px", height: "200px" }} onClick={goToM5}>preRegisterd</Button>
+                </div>
+                <div>
+                    <Button variant="contained" style={{ width: "200px", height: "200px" }} onClick={goToM8}>Registerd</Button>
+                </div>
+            </div>
         </Mainlayout>
     )
 }
